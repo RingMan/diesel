edb3461b350149cf09e5077a8014e1d6dbb1e1b0
David Kettering <david.ringman@gmail.com>
2013-02-15 09:20:29 -0600

    Add lein project scaffolding

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to diesel
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/diesel/core_test.clj
@@ -0,0 +1,7 @@
+(ns diesel.core-test
+  (:use clojure.test
+        diesel.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
