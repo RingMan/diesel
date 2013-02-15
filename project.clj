782948ada9436c33db3821cc262098b51ecb82e3
David Kettering <david.ringman@gmail.com>
2013-02-15 14:45:53 -0600

    Add initial midje facts

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject diesel "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "diesel = di-es-el = DSL helper library"
+  :url "https://github.com/RingMan/diesel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5-beta1"]]
+                   :plugins [[lein-midje "3.0-beta1"]]}})
--- test/diesel/core_test.clj
+++ test/diesel/core_test.clj
@@ -1,7 +1,25 @@
 (ns diesel.core-test
-  (:use clojure.test
+  (:use midje.sweet
         diesel.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact (mk-prop -k- -v-) => {-k- -v-})
+
+(fact (mk-unit-prop -k- -v- -u-) => {-k- {:units -u- :val -v-}})
+
+(facts
+  "about mk-pct-prop"
+  (let [pct 34 pct-str "34.0 percent"]
+    (mk-pct-prop -k- pct :bad-units) => (throws java.lang.AssertionError
+                                                "Assert failed: (pct-units u)")
+    (mk-pct-prop -k- (/ pct 100) :dec) => {-k- pct-str}
+    (mk-pct-prop -k- pct%) => {-k- pct-str}
+    (mk-pct-prop -k- pct :percent) => {-k- pct-str}
+    (mk-pct-prop -k- pct :pct) => {-k- pct-str}))
+
+(fact (mk-loc-prop -k- -lat- -lon-) => {-k- {:lat -lat- :lon -lon-}})
+
+(fact
+  (mk-prop-makers mk-prop age weight) =expands-to=>
+  (do
+    (def age (clojure.core/partial mk-prop :age))
+    (def weight (clojure.core/partial mk-prop :weight))))
