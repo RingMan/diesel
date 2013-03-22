7ed58daf85770da12798ce78108a4b8489fa3846
David Kettering <david.ringman@gmail.com>
2013-03-22 18:28:46 -0500

    Add more property types and helper macros
    Add support for boolean, vecto, and map properties.  Add helper
    macros to generate 'dynamic' properties and entity maps.

--- src/diesel/core.clj
+++ src/diesel/core.clj
@@ -3,8 +3,29 @@
 
 (def pct-units #{:pct :percent :dec})
 
+(defn mk-map* [m args]
+  (let [[x & xs] args]
+    (cond
+      (nil? args) m
+      (nil? x) (recur m xs)
+      (map? x) (recur (merge m x) xs)
+      (coll? x) (recur m (concat x xs))
+      (fn? x) (recur (x m) xs)
+      (keyword? x) (recur (assoc m x (first xs)) (rest xs))
+      :else (throw (RuntimeException.
+                     (str "Illegal arg to mk-map*: " x))))))
+
 (defn mk-prop [k v]
   {k v})
+
+(defn mk-bool-prop
+  ([k] {k true})
+  ([k v] {k (boolean v)}))
+
+(defn mk-map-prop [k & body]
+  {k (mk-map* {} body)})
+
+(defn mk-vec-prop [k & xs] {k (vec xs)})
 
 (defn mk-unit-prop [k v u]
   {k {:val v :units u}})
@@ -24,6 +45,15 @@
 (defmacro def-props [& syms]
   `(mk-prop-makers mk-prop ~@syms))
 
+(defmacro def-bool-props [& syms]
+  `(mk-prop-makers mk-bool-prop ~@syms))
+
+(defmacro def-map-props [& syms]
+  `(mk-prop-makers mk-map-prop ~@syms))
+
+(defmacro def-vec-props [& syms]
+  `(mk-prop-makers mk-vec-prop ~@syms))
+
 (defmacro def-pct-props [& syms]
   `(mk-prop-makers mk-pct-prop ~@syms))
 
@@ -32,6 +62,78 @@
 
 (defmacro def-loc-props [& syms]
   `(mk-prop-makers mk-loc-prop ~@syms))
+
+(defn properties [& body]
+  (mk-map* {} body))
+
+(defn property [id & body]
+  {id (mk-map* {} body)})
+
+(defn mk-map-form [{:keys [id tag]} sym]
+  `(mk-map* ~(merge {}
+                    (when tag {:tag (keyword sym)})
+                    (when id {id 'id})) ~'body))
+
+(defmacro def-dyn-props
+  "Creates functions or function aliases for defining 'dynamic' properties
+  (i.e., those where the property id is passed as an argument).  The macro
+  accepts one or more symbols possibly preceded by an options map.  If
+  the :tag option is truthy, generated maps will have a :tag key whose
+  value matches the function name.  If :id is supplied, its value will be
+  used as an ID key in generated maps.
+
+  Example:
+    (def-dyn-props prop1 prop2)
+    (def-dyn-props {:tag true} prop3 prop4)
+    (def-dyn-props {:id :name} prop5 prop6)
+    (def-dyn-props {:id :id, :tag :truthy} prop7)
+
+    (prop1 :age 4 :dob 2001) => {:prop1 {:age 4 :dob 2001}}
+    (prop3 :age 4 :dob 2001) => {:prop1 {:age 4 :dob 2001 :tag :prop3}}
+    (prop5 :fred :age 4 :dob 2001) => {:prop1 {:age 4 :dob 2001 :name :fred}}
+    (prop7 :fred :age 4 :dob 2001) =>
+      {:prop1 {:age 4 :dob 2001 :id :fred :tag :prop7}}"
+  [opt & syms]
+  (let [{:keys [id tag]} opt
+        syms (if (map? opt) syms (cons opt syms))]
+    (if (or id tag)
+      (cons 'do
+            (for [sym syms]
+              `(defn ~sym [~'id & ~'body]
+                 {~'id ~(mk-map-form opt sym)})))
+      (cons 'do
+            (for [sym syms]
+              `(def ~sym property))))))
+
+(defmacro def-entity-maps
+  "Creates functions for creating maps representing an entity.  The macro
+  accepts one or more symbols possibly preceded by an options map.  See
+  def-dyn-props for details
+
+  Example:
+    (def-entity-props {:tag 1 :id :id} person)
+
+    (person :fred :age 4 :dob 2001) =>
+      {:tag :person :id :fred :age 4 :dob 2001}"
+  [opt & syms]
+  (let [{:keys [id tag]} opt
+        syms (if (map? opt) syms (cons opt syms))]
+    (cons 'do
+          (for [sym syms]
+            `(defn ~sym ~(if id '[id & body] '[& body])
+               ~(mk-map-form opt sym))))))
+
+(defmacro def-entity-macros
+  "Creates an entity-generating macro for each symbol in syms.
+  For example (def-entity-macros scenario) creates a macro like
+
+  (defmacro defwidget [id & body]
+    `(def ~id (widget ~(name id) ~@body)))"
+  [& syms]
+  (cons 'do
+        (for [sym syms]
+          `(defmacro ~(symbol (str "def" (name sym))) [~'id & ~'body]
+             `(def ~~'id ('~~sym ~~'(keyword (name id)) ~@~'body))))))
 
 (def N :N)
 (def S :S)
@@ -50,37 +152,6 @@
   {:lon
    (apply str (interpose " " (map #(if (keyword %) (name %) %) args)))})
 
-(defn mk-map* [m args]
-  (let [[x & xs] args]
-    (cond
-      (nil? args) m
-      (nil? x) (recur m xs)
-      (map? x) (recur (merge m x) xs)
-      (coll? x) (recur m (concat x xs))
-      (fn? x) (recur (x m) xs)
-      (keyword? x) (recur (assoc m x (first xs)) (rest xs))
-      :else (throw (RuntimeException.
-                     (str "Illegal arg to mk-map*: " x))))))
-
-(defn properties [& body]
-  (mk-map* {} body))
-
-;Next two funcs define some generic syntax useful for creating lists of
-;named items
-;Example:
-;(defn widgets [& body]
-;  {:widgets (mk-map* {} body)})
-;
-;(widgets (item :toaster :cost 3 :qty 2) (item :phone :cost 5 :qty 5))
-;
-;=> {:widgets {:toaster {:cost 3 :qty 2}
-;              :phone   {:cost 5 :qty 5}}}
-(defn item [id & body]
-  {id (mk-map* {} body)})
-
-(defn entry [id & body]
-  {id (mk-map* {} body)})
-
 (defn alter-in
   "Merges args into map
   If an arg is a keyword, the next arg is taken to be its value
@@ -88,16 +159,3 @@
   Function args are invoked with the current map passed in"
   [m & args]
   (mk-map* m args))
-
-(defmacro def-entity-macros
-  "Creates an entity-generating macro for each symbol in syms.
-  For example (def-entity-macros scenario) creates a macro like
-
-  (defmacro defscenario [id & body]
-    `(def ~id (scenario ~(name id) ~@body)))"
-  [& syms]
-  (cons 'do
-        (for [sym syms]
-          `(defmacro ~(symbol (str "def" (name sym))) [~'id & ~'body]
-             `(def ~~'id ('~~sym ~~'(keyword (name id)) ~@~'body))))))
-
