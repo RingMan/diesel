3da39c60c7848b488daf4685e566300d6a74d54b
David Kettering <david.ringman@gmail.com>
2013-03-27 21:54:08 -0500

    Prepare project file for Clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject diesel "0.1.0-SNAPSHOT"
+(defproject diesel "0.1.0"
   :description "diesel = di-es-el = DSL helper library"
   :url "https://github.com/RingMan/diesel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/RingMan/diesel"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.5-beta1"]]
                    :plugins [[lein-midje "3.0-beta1"]]}})
