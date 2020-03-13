a75466f035c9865b72b7182edb59a733634a9fed
David Kettering <david.ringman@gmail.com>
2020-03-13 10:17:45 -0500

    Add tests for def-entity-maps, def-entity-props, def-entity-macros

--- test/diesel/core_test.clj
+++ test/diesel/core_test.clj
@@ -83,3 +83,84 @@
   (def-loc-props position gps-loc) =expands-to=>
   (diesel.core/mk-prop-makers diesel.core/mk-loc-prop position gps-loc))
 
+;define two properties of books
+(def-props author pages)
+;generate functions to create book entities
+(def-entity-maps book)
+(def-entity-maps {:tag 1} bookWithTag)
+(def-entity-maps {:id :title} bookWithId)
+(def-entity-maps {:id :entity, :tag true} bookWithIdAndTag)
+
+(fact (author -a-) => {:author -a-})
+(fact (pages -p-) => {:pages -p-})
+
+(fact "book adds nothing extra to Entity properties"
+      (book (pages -p-) (author -a-)) => (just {:author -a- :pages -p-}))
+
+(fact "bookWithTag adds :tag to Entity properties"
+      (bookWithTag (pages -p-) (author -a-)) =>
+      (just {:tag :bookWithTag :author -a- :pages -p-}))
+
+(fact "bookWithId takes id as first arg and adds :title to Entity properties"
+      (bookWithId -id- (pages -p-) (author -a-)) =>
+      (just {:title -id- :author -a- :pages -p-}))
+
+(fact "bookWithIdAndTag takes id as first arg and adds :entity and :tag
+      to Entity properties"
+      (bookWithIdAndTag -id- (pages -p-) (author -a-)) =>
+      (just {:tag :bookWithIdAndTag :entity -id- :author -a- :pages -p-}))
+
+;define two properties of cars
+(def-props model vin)
+;generate functions to create properties whose value is a car Entity
+(def-entity-props car)
+(def-entity-props {:tag 1} carWithTag)
+(def-entity-props {:id :nickname} carWithId)
+(def-entity-props {:id :handle, :tag true} carWithIdAndTag)
+
+(fact (model -m-) => {:model -m-})
+(fact (vin -v-) => {:vin -v-})
+
+(fact "car takes id as first arg but adds nothing extra to Entity properties"
+      (car -id- (vin -v-) (model -m-)) => (just {-id- {:model -m- :vin -v-}}))
+
+(fact "carWithTag takes id as first arg and adds :tag to Entity properties"
+      (carWithTag -id- (vin -v-) (model -m-)) =>
+      (just {-id- {:tag :carWithTag :model -m- :vin -v-}}))
+
+(fact "carWithId takes id as first arg and adds :nickname to Entity properties"
+      (carWithId -id- (vin -v-) (model -m-)) =>
+      (just {-id- {:nickname -id- :model -m- :vin -v-}}))
+
+(fact "carWithIdAndTag takes id as first arg and adds :handle and :tag
+      to Entity properties"
+      (carWithIdAndTag -id- (vin -v-) (model -m-)) =>
+      (just {-id- {:tag :carWithIdAndTag :handle -id- :model -m- :vin -v-}}))
+
+;macro is only suitable for functions that take id as first arg, but fn
+;can return either an Entity or an Entity Property
+
+(def-entity-macros bookWithId bookWithIdAndTag)
+
+(defbookWithId myBkId (author :me) (pages 100))
+(fact myBkId => (just {:title :myBkId :author :me :pages 100}))
+
+(defbookWithIdAndTag myBkIdTag (author :me) (pages 100))
+(fact myBkIdTag => (just {:tag :bookWithIdAndTag :entity :myBkIdTag :author :me :pages 100}))
+
+;how you might generate macros for a DSL describing car entities
+;macro is suitable to use with any function created by def-entity-props
+
+(def-entity-macros car carWithTag carWithId carWithIdAndTag)
+
+;how client code might invoke your generated macros
+
+(defcar myCar (vin 123) (model :T))
+(defcarWithTag myCarTag (vin 123) (model :T))
+(defcarWithId myCarId (vin 123) (model :T))
+(defcarWithIdAndTag myCarIdTag (vin 123) (model :T))
+
+(fact myCar => (just {:myCar {:vin 123 :model :T}}))
+(fact myCarTag => (just {:myCarTag {:tag :carWithTag :vin 123 :model :T}}))
+(fact myCarId => (just {:myCarId {:nickname :myCarId :vin 123 :model :T}}))
+(fact myCarIdTag => (just {:myCarIdTag {:handle :myCarIdTag :tag :carWithIdAndTag :vin 123 :model :T}}))
